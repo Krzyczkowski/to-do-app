fd9a2a528c5be423ccd827cf8d3602ab86b046fd
K4am <krzyczkowski.wiktor00@gmail.com>
2023-06-13 10:50:36 +0200

    u

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Home from './pages/Home';
 import TodoList from './pages/TodoList';
 import Contact from './pages/Contact';
 import './App.css';
+import NotFound from './pages/NotFound';
 function App() {
   return (
     <Router>
       <div>
         <Navigation />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todo" element={<TodoList />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="to-do-app/" element={<Home />} />
+          <Route path="to-do-app/todo" element={<TodoList />} />
+          <Route path="to-do-app/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+function NotFound() {
+  return (
+    <center>
+    <div>
+      <h1>404 NOT FOUND</h1>
+    </div>
+    </center>
+  );
+}
+
+export default NotFound;
